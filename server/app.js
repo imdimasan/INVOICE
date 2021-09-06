6a4dab43c23599d58db1dfc54ca73b3ff4045295
WebStarLink <info@starlink.by>
2021-09-06 22:52:11 +0300

    Updated app.js server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 const PORT = config.get("port") || 5000;
@@ -14,6 +15,13 @@ app.use(express.json({ extended: true }));
 app.use("/api/auth", AUTHROUTES);
 app.use("/api/link", LINKS);
 app.use("/api/client", CLIENTS);
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join("cd .. &&", "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("cd .. &&", "client", "build", "index.hml"));
+  });
+}
 
 // Коннект к БД
 async function start() {
