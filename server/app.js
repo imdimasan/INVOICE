7f78ed2d4fd57600e453b9f259cdc990e11b5faf
WebStarLink <info@starlink.by>
2021-09-07 22:09:39 +0300

    Fixed history

--- server/app.js
+++ server/app.js
@@ -19,7 +19,9 @@ app.use("/api/client", CLIENTS);
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(path.join(__dirname, "..", "client", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.hml"));
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
   });
 }
 
